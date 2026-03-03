15a330315be956be3dd8fc6bf56e16dc0072c39f
unknown <tech47488@gmail.com>
2026-03-03 15:50:02 +0530

    added UC3

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,13 +1,9 @@
-public class OopsBannerApp{
-	public static void main(String[]args){
-
-        System.out.println(" *****    *****    *****    ***** ");
-        System.out.println("*     *  *     *  *     *  *      ");
-        System.out.println("*     *  *     *  *     *  *      ");
-        System.out.println("*     *  *     *  *****     ***** ");
-        System.out.println("*     *  *     *  *              *");
-        System.out.println("*     *  *     *  *              *");
-        System.out.println(" *****    *****   *         ***** ");
-
+public class OopsBannerApp {
+    public static void main(String[] args) {
+        System.out.println(" OOO    OOO   PPPP   SSSS ");
+        System.out.println("O   O  O   O  P   P  S     ");
+        System.out.println("O   O  O   O  PPPP    SSS   ");
+        System.out.println("O   O  O   O  P          S  ");
+        System.out.println(" OOO    OOO   P      SSSS   ");
     }
 }
