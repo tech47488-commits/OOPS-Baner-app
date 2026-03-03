54c5dc19bfb8e82d3cec91ba3e3d32d1bd82a5dc
tech47488-commits <tech47488@gmail.com>
2026-03-03 16:04:17 +0530

    Refactor OopsBannerApp to Main and update output
    Refactored the OopsBannerApp class to Main and updated the output to use String.join for better formatting.

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,9 +1,13 @@
-public class OopsBannerApp {
+public class Main {
     public static void main(String[] args) {
-        System.out.println(" OOO    OOO   PPPP   SSSS ");
-        System.out.println("O   O  O   O  P   P  S     ");
-        System.out.println("O   O  O   O  PPPP    SSS   ");
-        System.out.println("O   O  O   O  P          S  ");
-        System.out.println(" OOO    OOO   P      SSSS   ");
+
+        // Using String.join to join parts with space
+        System.out.println(String.join(" ", "   ***   ", " ***** "));
+        System.out.println(String.join(" ", " **   ** ", " **     "));
+        System.out.println(String.join(" ", " **   ** ", " **     "));
+        System.out.println(String.join(" ", " **   ** ", " *****  "));
+        System.out.println(String.join(" ", " **   ** ", "     ** "));
+        System.out.println(String.join(" ", " **   ** ", "     ** "));
+        System.out.println(String.join(" ", "   ***   ", " *****  "));
     }
 }
